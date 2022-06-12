45f9663cbd0794caa1cc13d14f7bb01269d6fbe4
danimelchor <dmh672@gmail.com>
2022-06-12 16:58:50 -0700

    updated installation

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# Check nvim is installed
+if ! [ -x "$(command -v nvim)" ]; then
+  brew install nvim
+fi
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 # Get dotfiles paths
 nvim_dir="$(pwd)/nvim"
 
@@ -6,6 +12,7 @@ cd ~/.config
 
 # Make copies just in case
 [ -d "nvim" ] && mv ./nvim ./nvim.old 
+rm nvim
 
 # Create symlinks
 ln -s $nvim_dir

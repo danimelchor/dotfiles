fb71e824f61bc998cdfdcece3e1ededc394014ef
Daniel Melchor <dmelchor@stripe.com>
2022-06-13 19:28:57 -0700

    update install cmd

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ prompt() {
 
 # nvim
 prompt "Do you want to install nvim? [Y/n] "
-if [[ $result ]] then
+if [ "$result" = true ]; then
   brew install nvim
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
   nvim_dir="$(pwd)/nvim"

2bbce30ed0dac808f40868ca9a0c6bf2fb869108
Daniel Melchor <dmelchor@stripe.com>
2022-06-14 10:13:45 -0700

    zshrc install test

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -38,7 +38,7 @@ require('neo-tree').setup({
   },
   filesystem = {
     filtered_items = {
-      visible = false,
+      visible = true,
       hide_dotfiles = false,
       hide_gitignored = false,
       hide_by_name = {

80aa3662c0be586c597f504da5d3a86cf20456df
Daniel Melchor <dmelchor@stripe.com>
2022-06-13 19:32:44 -0700

    trying to get install to work

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ if [ "$result" = true ]; then
   brew install nvim
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
   nvim_dir="$(pwd)/nvim"
+  cd ~/.config
   [ -d "nvim" ] && mv ./nvim ./nvim.old 
   rm nvim
   ln -s $nvim_dird ~/.config

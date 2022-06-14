26190e94f70aa1adedc5d07bc70b061dc4bc97de
Daniel Melchor <dmelchor@stripe.com>
2022-06-14 10:14:37 -0700

    fixed install move command

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ if [ "$result" = true ]; then
   rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
   [ -d ~./config/nvim.old ] && rm -rf ~/.config/nvim.old
-  [ -d ~/.config/nvim ] && mv -r -f ~/.config/nvim ~/.config/nvim.old 
+  [ -d ~/.config/nvim ] && mv -f ~/.config/nvim ~/.config/nvim.old 
   cp -R $INSTALL_DIR/nvim ~/.config/nvim
   mkdir -p ~/.config/nvim/undo
 fi

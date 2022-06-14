11c43ea674d547512ea809cfa66d28f33fdd8511
danimelchor <dmh672@gmail.com>
2022-06-14 00:56:14 -0700

    included zshrc

--- install.sh
+++ install.sh
@@ -8,16 +8,21 @@ prompt() {
   done
 }
 
+INSTALL_DIR=$( dirname -- "$0"; )
+
 # nvim
-prompt "Do you want to install nvim? [Y/n] "
+prompt "Do you want to install my nvim config?"
 if [ "$result" = true ]; then
   ! [ -x "$(command -v nvim)" ] && brew install nvim
   rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-  nvim_dir="$(pwd)/nvim"
-  cd ~/.config
-  rm -rf ./nvim.old
-  [ -d ./nvim ] && mv -f ./nvim ./nvim.old 
-  ln -s $nvim_dir
-  mkdir -p ./nvim/undo
+  rm -rf ~/.config/nvim.old
+  [ -d ~/.config/nvim ] && mv -f ~/.config/nvim ~/.config/nvim.old 
+  ln -s $INSTALL_DIR/nvim ~/.config/nvim
+  mkdir -p ~/.config/nvim/undo
+fi
+
+# zshrc
+prompt "Do you want to install my .zshrc?"
+if [ "$result" = true ]; then
 fi

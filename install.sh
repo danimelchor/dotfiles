13e821a03688ddf570823b0c209c1696ed14849b
Daniel Melchor <dmelchor@stripe.com>
2022-06-13 19:38:01 -0700

    trying to get install to work

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ prompt() {
 prompt "Do you want to install nvim? [Y/n] "
 if [ "$result" = true ]; then
   brew install nvim
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+  packer_path=~/.local/share/nvim/site/pack/packer/start/packer.nvim
+  [ -f "$packer_path" ] rm "$packer_path"
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim $packer_path
   nvim_dir="$(pwd)/nvim"
   cd ~/.config
   [ -d "nvim" ] && mv ./nvim ./nvim.old 

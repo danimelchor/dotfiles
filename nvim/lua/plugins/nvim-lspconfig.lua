3f1f4c7d3b14c1548911fffd2576d2a3ab696a77
Daniel Melchor <dmelchor@stripe.com>
2022-06-15 16:13:54 -0700

    switched python lsp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(
   protocol.make_client_capabilities()
 )
 
-require'lspconfig'.pyright.setup({
+require'lspconfig'.pylsp.setup({
   capabilities = capabilities
 })
 

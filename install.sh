6786ca75d7dd24daf3c7d80b4869e7ab37882c8e
danimelchor <dmh672@gmail.com>
2022-06-19 16:12:13 -0700

    better install prompts;gp

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 prompt() {
-  echo "\033[1m\033[0;93m$1 [Y/n] \033[0m\c"
+  echo "\033[1m\033[93m$1 [Y/n] \033[0m\c"
   read yn
   if [ "$yn" = "" ]; then
     yn='Y'
@@ -18,21 +18,30 @@ prompt "Do you want to install my nvim config?"
 if [ "$result" = true ]; then
   ! [ -x "$(command -v nvim)" ] && brew install nvim
   rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+  git clone -q --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
   [ -d ~/.config/nvim.old ] && rm -rf ~/.config/nvim.old
   [ -d ~/.config/nvim ] && mv -f ~/.config/nvim ~/.config/nvim.old 
   [ -h ~/.config/nvim ] && rm ~/.config/nvim
   ln -s $INSTALL_DIR/nvim ~/.config/nvim
   mkdir -p ~/.config/nvim/undo
+  echo "\033[1m\033[92mDone.\033[0m\n"
 fi
 
 # zshrc
 prompt "Do you want to install my .zshrc?"
 if [ "$result" = true ]; then
-  ! [ -x "$(command -v p10k)" ] && rm -rf ~/powerlevel10k && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+  ! [ -x "$(command -v p10k)" ] && rm -rf ~/powerlevel10k && git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
   [ -f ~/.zshrc.old ] && rm ~/.zshrc.old
-  [ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.old
+  [ -f ~/.p10k.zsh.old ] && rm ~/.p10k.zsh.old
+
+  [ -f ~/.p10k.zsh ] && mv -f ~/.p10k.zsh ~/.p10k.zsh.old 
+  [ -f ~/.zshrc ] && mv -f ~/.zshrc ~/.zshrc.old
+
   [ -h ~/.zshrc ] && rm ~/.zshrc
+  [ -h ~/.p10k.zsh ] && rm ~/.p10k.zsh
+
   ln -s $INSTALL_DIR/zshrc/.zshrc ~/.zshrc
   ln -s $INSTALL_DIR/zshrc/.p10k.zsh ~/.p10k.zsh
+  echo "\033[1m\033[92mDone.\033[0m\n"
 fi

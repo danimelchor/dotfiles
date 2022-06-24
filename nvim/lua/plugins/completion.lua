1c2205b3d316aa3e88354a217f3341eb218d3365
Daniel Melchor <dmelchor@stripe.com>
2022-06-24 15:33:36 -0700

    cleaned up cmp config

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -3,24 +3,6 @@ if not status_ok then
   return
 end
 
-local next = cmp.mapping(function(fallback)
-  if cmp.visible() then
-    cmp.select_next_item()
-  elseif vim.fn["vsnip#available"]() == 1 then
-    vim.fn.feedkeys(t("<Plug>(vsnip-expand-or-jump)"), "")
-  else
-    fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-  end
-end, { "i", "s" })
-
-local prev = cmp.mapping(function()
-  if cmp.visible() then
-    cmp.select_prev_item()
-  elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-    vim.fn.feedkeys(t("<Plug>(vsnip-jump-prev)"), "")
-  end
-end, { "i", "s" })
-
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -28,8 +10,9 @@ cmp.setup({
     end,
   },
   mapping = {
-    ['<Tab>'] = next,
-    ["<S-Tab>"] = prev
+    ['<Tab>'] = cmp.select_next_item(),,
+    ['<S-Tab>'] = cmp.select_prev_item(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true })
   },
   sources = {
     { name = 'nvim_lsp' },

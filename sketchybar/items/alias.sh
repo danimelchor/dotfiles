dc6b148097c7247525eb37ee1e37a6302b5efea7
danimelchor <dmh672@gmail.com>
2023-03-03 14:46:17 -0500

    cron refreshes every 60 seconds now

--- sketchybar/items/alias.sh
+++ sketchybar/items/alias.sh
@@ -1,4 +1,5 @@
 sketchybar --add alias "Cron,Item-0" e \
            --rename "Cron,Item-0" cron \
            --set cron width=30 \
-                      padding_left=7
+                      padding_left=7 \
+                      alias.update_freq=60

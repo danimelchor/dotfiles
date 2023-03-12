f20d31f136db55f88efd6f66a00ae662913109dc
danimelchor <dmh672@gmail.com>
2023-03-11 20:35:04 -0500

    bigger todo icon

--- sketchybar/items/todo.sh
+++ sketchybar/items/todo.sh
@@ -2,7 +2,7 @@
 
 sketchybar --add item todo q                                                \
            --set todo update_freq=120                                            \
-                       label.font="Hack Nerd Font:Regular:12.0"                     \
+                       label.font="Hack Nerd Font:Regular:16.0"                     \
                        icon.font="$FONT:Bold:12"                                \
                        script="$PLUGIN_DIR/todo.sh"                             \
                        background.height=20                                         \

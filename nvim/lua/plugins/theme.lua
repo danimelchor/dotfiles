572a47a01f43df6d81b3397f4b770508809924de
danimelchor <dmh672@gmail.com>
2023-03-29 20:33:59 -0400

    removed old alpha stuff

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,10 +1,6 @@
 require('catppuccin').setup()
 vim.cmd("colorscheme catppuccin")
 
-vim.cmd([[ hi DashboardHeader ctermbg=0 guifg=#fab387 ]])
-vim.cmd([[ hi DashboardCenter ctermbg=0 guifg=#89b4fa ]])
-vim.cmd([[ hi DashboardShortcut ctermbg=0 guifg=#89dceb ]])
-
 vim.opt.termguicolors = true
 vim.cmd("silent! syntax enable")
 vim.cmd("silent! hi Normal guibg=NONE ctermbg=NONE")

8980bdaff38e1d68c963c689b88e7ee255e364c1
Daniel Melchor Hachuel <dmelchor@stripe.com>
2023-08-14 11:51:13 -0400

    added sketchybvar

---
+++ sketchybar/items/alias.sh
@@ -0,0 +1,5 @@
+sketchybar --add alias "MeetingBar,Item-0" e \
+           --rename "MeetingBar,Item-0" cron \
+           --set cron width=30 \
+                      padding_left=7 \
+                      alias.update_freq=60

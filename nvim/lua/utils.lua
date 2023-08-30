aef83312425584469b3d6a88947adf570093d938
Daniel Melchor Hachuel <dmelchor@stripe.com>
2023-08-29 23:52:32 -0400

    remove print

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -6,14 +6,13 @@ function M.exists(file)
       file = os.getenv("HOME") .. file:sub(2)
    end
 
-   local ok, err, code = os.rename(file, file)
+   local ok, _, code = os.rename(file, file)
    if not ok then
       if code == 13 then
          -- Permission denied, but it exists
          return true
       end
    end
-   if not ok then print(err) end
    return ok
 end
 

22c0dd62e62f49fe5d2a15422863720eea864484
Daniel Melchor Hachuel <dmelchor@stripe.com>
2023-09-06 01:48:25 -0400

    copilot config

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -8,5 +8,13 @@ require('copilot').setup({
   suggestion = {
     enabled = true,
     auto_trigger = true,
+    keymap = {
+      accept = false,
+      accept_word = false,
+      accept_line = false,
+      next = false,
+      prev = false,
+      dismiss = false,
+    }
   }
 })

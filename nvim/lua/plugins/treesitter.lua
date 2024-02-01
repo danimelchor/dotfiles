d1f14b4341e1949264ca0035edf77b8b8f1a695b
danimelchor <dmelchor@pm.me>
2024-02-01 18:39:01 -0500

    treesitter move shorcuts

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -102,6 +102,24 @@ return {
               ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
             }
           },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+              ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+              ["]c"] = { query = "@class.outer", desc = "Next class start" },
+              ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+              ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+            },
+            goto_previous_start = {
+              ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+              ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+              ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+              ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+              ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+            },
+          },
         },
       })
     end

98f9b192e40109f7043f5e414351044d23ab826a
Daniel Melchor Hachuel <dmelchor@stripe.com>
2024-02-01 17:14:02 -0500

    text objects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -44,20 +44,66 @@ return {
         auto_install = true,
         highlight = {
           enable = true,
-          additional_vim_regex_highlighting = false
         },
         indent = {
           enable = true,
         },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        }
       }
 
       local parser_config = nvim_treesitter_parsers.get_parser_configs()
       parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
     end,
+    build = ":TSUpdate",
     event = "BufEnter",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-context",
       "nvim-treesitter/nvim-treesitter-textobjects",
     }
+  },
+
+  -- Tressitter text objects
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        textobjects = {
+          select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+              ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+              ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+              ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+              ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+              ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+              ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+              ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+              ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+              ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+              ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+              ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+            }
+          },
+        },
+      })
+    end
   },
 }

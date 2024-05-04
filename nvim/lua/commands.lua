1c010bad057aee06931e249fe6fca2344f26661a
danimelchor <dmelchor@pm.me>
2024-05-04 11:37:50 -0400

    fix vert help

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -15,12 +15,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 local VertHelp = vim.api.nvim_create_augroup("VertHelp", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
-    if vim.bo.filetype == "help" then
+    if vim.api.nvim_buf_get_option(0, "buftype") == "help" then
       vim.api.nvim_command("wincmd L")
     end
   end,
   group = VertHelp,
-  pattern = "help",
+  pattern = { "*.txt" },
 })

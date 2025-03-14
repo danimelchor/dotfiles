192911bf2953413f319d7fa2a6a8398096054ed8
danimelchor <dmelchor@pm.me>
2025-03-13 21:47:57 -0400

    configs

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,49 +1,57 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local oil = require('oil')
-      oil.setup({
-        use_default_keymaps = false,
-        delete_to_trash = true,
-        skip_confirm_for_simple_edits = true,
-        keymaps = {
-          ["g?"] = "actions.show_help",
-          ["I"] = "actions.toggle_hidden",
-          ["<CR>"] = "actions.select",
-          ["-"] = "actions.parent",
-          ["_"] = "actions.open_cwd",
-          ["R"] = "actions.refresh",
-          ["<LEADER>n"] = "actions.close",
-          ["s"] = function()
-            require('oil.actions').select_vsplit.callback()
-            require('oil.actions').close.callback()
-          end,
-          ["S"] = function()
-            require('oil.actions').select_split.callback()
-            require('oil.actions').close.callback()
-          end,
-          ["H"] = function()
-            -- Function to add oil entry to harpoon
-            local Path = require("plenary.path")
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local oil = require("oil")
+			oil.setup({
+				use_default_keymaps = false,
+				delete_to_trash = true,
+				skip_confirm_for_simple_edits = true,
+				prompt_save_on_select_new_entry = false,
+				view_options = {
+					show_hidden = true,
+				},
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["R"] = "actions.refresh",
+					["<LEADER>n"] = "actions.close",
+					["s"] = function()
+						require("oil.actions").select_vsplit.callback()
+						require("oil.actions").close.callback()
+					end,
+					["S"] = function()
+						require("oil.actions").select_split.callback()
+						require("oil.actions").close.callback()
+					end,
+					["H"] = function()
+						-- Function to add oil entry to harpoon
+						local Path = require("plenary.path")
 
-            -- Get file under cursor
-            local entry = oil.get_cursor_entry()
-            local filename = entry and entry.name
-            local dir = oil.get_current_dir()
+						-- Get file under cursor
+						local entry = oil.get_cursor_entry()
+						local filename = entry and entry.name
+						local dir = oil.get_current_dir()
 
-            -- Add to harpoon
-            local listItem = {
-              context = { row = 1, col = 0 },
-              value = Path:new(dir .. filename):make_relative(vim.fn.getcwd()),
-            }
-            require("harpoon"):list():add(listItem)
-          end
-        }
-      })
+						-- Add to harpoon
+						local listItem = {
+							context = { row = 1, col = 0 },
+							value = Path:new(dir .. filename):make_relative(vim.fn.getcwd()),
+						}
+						require("harpoon"):list():add(listItem)
+					end,
+				},
+			})
 
-      vim.api.nvim_set_keymap('n', '<LEADER>n', '<Cmd>Oil<CR>', { noremap = true, silent = true, desc = 'Open Oil' })
-    end,
-  }
+			vim.api.nvim_set_keymap(
+				"n",
+				"<LEADER>n",
+				"<Cmd>Oil<CR>",
+				{ noremap = true, silent = true, desc = "Open Oil" }
+			)
+		end,
+	},
 }

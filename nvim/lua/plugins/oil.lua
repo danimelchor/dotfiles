8d5d740a021b333e15d420c5021f745f76c835cf
Daniel Melchor Hachuel <dmelchor@stripe.com>
2025-06-18 11:39:03 -0400

    prompt on save

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -8,7 +8,6 @@ return {
 				use_default_keymaps = false,
 				delete_to_trash = true,
 				skip_confirm_for_simple_edits = true,
-				prompt_save_on_select_new_entry = false,
 				view_options = {
 					show_hidden = true,
 				},
